b40420d74f452f24b9b13765548a0d66ec41e413
Вадим Шуваев <shuvaev.v.6@outlook.com>
2021-11-27 20:07:26 +0700

    Список изменений: Дописан main

---
+++ Sem 3 Lab 3 C_Sharp/DebitAccount.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sem_3_Lab_3_C_Sharp
+{
+    public class DebitAccount
+    {
+        private int DebitAccountID;
+        private int Currency;
+        private double Balance;
+
+        
+            public enum CurrTypes
+            {
+                RUB = 0,
+                USD,
+                EUR
+            };
+
+            public List<double> CURRENCY_EXCHANGE_RATE = new(){1.0, 0.014, 0.012};
+
+        
+        public static int DA_account;
+        public DebitAccount(){
+            this.DebitAccountID = DA_account;
+            this.Currency = (int)CurrTypes.RUB;
+            this.Balance = 0.0;
+            DA_account++;
+        }
+
+        public DebitAccount(int curr, double bal)
+        {
+            this.DebitAccountID = DA_account;
+            this.Currency = curr;
+            this.Balance = bal;
+            DA_account++;
+        }
+
+        public int GetDebitAccountID()
+        {
+            return DebitAccountID;
+        }
+
+        public int GetCurrency()
+        {
+            return Currency;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+        public void Replemishment(int add, int curr = 0)
+        {
+            this.Balance += add * CURRENCY_EXCHANGE_RATE[Currency] / CURRENCY_EXCHANGE_RATE[curr];
+        }
+
+        public void Debting(int dec, int curr = 0)
+        {
+            double k = Balance;
+            this.Balance -= dec * CURRENCY_EXCHANGE_RATE[Currency] / CURRENCY_EXCHANGE_RATE[curr];
+            if (Balance < 0)
+            {
+                Console.WriteLine("Ошибка! На балансе недостаточно средств!");
+                this.Balance = k;
+            }
+        }
+
+        public void Out()
+        {
+            Console.WriteLine($"DebitAccountID: {this.DebitAccountID}");
+            Console.WriteLine($"Currency: {this.Currency}");
+            Console.Write($"Balance: {this.Balance}");
+            switch (Currency)
+            {
+                case 0:
+                    {
+                        Console.Write(" RUB\n");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.Write(" USD\n");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write(" EUR\n");
+                        break;
+                    }
+            }
+        }
+
+    }
+}
--- Sem 3 Lab 3 C_Sharp/Program.cs
+++ Sem 3 Lab 3 C_Sharp/Program.cs
@@ -4,9 +4,43 @@ namespace Sem_3_Lab_3_C_Sharp
 {
     internal class Program
     {
+        public static void User(ref DebitAccount dbt_acc)
+        {
+            int choice;
+            Console.Write("Выберите действие со счетом: \n1. Пополнить счет.\n2. Снять деньги со счета.\n: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        int curr, add;
+                        dbt_acc.Out();
+                        Console.Write("Валюта:\n1. RUB.\n2. USD.\n3. EUR.\n: ");
+                        curr = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Сумма для пополнения\n: ");
+                        add = Convert.ToInt32(Console.ReadLine());
+                        dbt_acc.Replemishment(add, curr-1);
+                        dbt_acc.Out();
+                        break;
+                    }
+                    case 2:
+                    {
+                        int curr, dec;
+                        dbt_acc.Out();
+                        Console.Write("Валюта:\n1. RUB.\n2. USD.\n3. EUR.\n: ");
+                        curr = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Сумма для пополнения\n: ");
+                        dec = Convert.ToInt32(Console.ReadLine());
+                        dbt_acc.Debting(dec, curr - 1);
+                        dbt_acc.Out();
+                        break;
+                    }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DebitAccount Alice = new DebitAccount(1,1500);
+            User(ref Alice);
         }
     }
 }
